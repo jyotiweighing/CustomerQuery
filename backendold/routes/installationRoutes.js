a8d0e9f402335e1a67aa2514837ca8be9c016be8
chhavichaturvedi011 <chhavichaturvedi011@gmail.com>
2026-09-22 10:32:57 +0530

    (update)

--- backendold/routes/installationRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const installationController = require("../controllers/installationController");
-
-router.put("/:id", installationController.updateInstallation);
-router.post("/create", installationController.createInstallation);
-router.get("/getinstall", installationController.getInstallations);
-module.exports = router;
--- backendold/routes/reportRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const reportController = require('../controllers/reportController');
-
-
-router.get('/analytics', reportController.getReportsAnalytics);
-router.get('/getDashboardData',reportController.getDashboardStats);
-module.exports = router;
